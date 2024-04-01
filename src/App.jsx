75dba928ae7fbfa2e95cd2aea7a3108f19119e70
Sushant Singh <sushantbibhu@gmail.com>
2024-04-01 11:40:27 +0530

    Update dependencies, add new components and styles * Updated "tailwind.config.js" * Updated "vite.config.js" with new alias for "@" * Updated "src/index.css" with new base styles and color variables * Updated "src/App.jsx" to include the new Button component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import {} from "react";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-green-500">Hello</h1>
+      <Button className="ml-2">Click me</Button>
     </>
   );
 }
